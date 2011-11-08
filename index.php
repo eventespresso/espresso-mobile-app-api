9aa07330f47e44a9ddda81fe68c976c0c7704589
Seth Shoultes <seth@eventespresso.com>
2011-11-08 23:56:40 +0000

    Added some branding

--- index.php
+++ index.php
@@ -10,3 +10,15 @@ Copyright 2011  Event Espresso  (email : support@eventespresso.com)
 
 WARNING!! Do not upload/install to your plugin directory. Please view the readm.txt for installation instructions.
 */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>This site is being powered by <a href="http://eventespresso.com/" target="_blank">Event Espresso's</a> <a href="http://eventespresso.com/mobile-ticketing-apps/" target="_blank">mobile ticketing application</a>!</h1>
+</body>
+</html>
