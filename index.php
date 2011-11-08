9f4d5d21568d9e2cacc488cce29c991e57ef4fae
Seth Shoultes <seth@eventespresso.com>
2011-11-08 23:39:21 +0000

    Adding index file with version information. TODO: Add a readme.txt file with install instructions.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+Plugin Name: Espresso Services
+Plugin URI: http://www.eventespresso.com
+Description: Back-end services for Event Espresso mobile ticketing application
+Version: 1.1
+Author: Event Espresso
+Author URI: http://www.eventespresso.com
+Copyright 2011  Event Espresso  (email : support@eventespresso.com) 
+
+WARNING!! Do not upload/install to your plugin directory. Please view the readm.txt for installation instructions.
+*/
