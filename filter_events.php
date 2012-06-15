be9b35e7883f9565ec223b5ac43208819cb38258
Seth Shoultes <seth@eventespresso.com>
2012-06-15 15:41:24 +0000

    Adding latest changes from the app dev team

--- filter_events.php
+++ filter_events.php
@@ -6,23 +6,6 @@ include_once('../wp-includes/wp-db.php');
 //Uncomment the next line if using multiple event managers
 //$multi_user = true;
 
-//limit results
-$use_limit = true;
-
-//Events per page
-$perPage = 20;
-
-//Use page offset if using limits
-$use_offset = true;	//Set to false until next version of app is ready
-
-//Check page number
-if( !isset($_POST['page']) || $_POST['page']==""){
-	$page = 0;
-}else{
-    $page = $_POST['page'];
-}
-$offset = $page * $perPage;
-
 if( !isset($_POST['authenticate']) || $_POST['authenticate']==""){
 	die("<?xml version='1.0'?><events><error>1</error><desc>Service Not Accessible</desc></events>");
 }
@@ -36,10 +19,11 @@ if( !isset($_POST['username']) || $_POST['username']==""){
 }
 $username = $_POST['username'];
 
-if( !isset($_POST['period']) || $_POST['period']==""){
-	die("<?xml version='1.0'?><events><error>1</error><desc>Fetch period is missing</desc></events>");
+if( !isset($_POST['substring']) || $_POST['substring']==""){
+	die("<?xml version='1.0'?><events><error>1</error><desc>Substring is empty</desc></events>");
 }
-$period = $_POST['period'];
+$substring = $_POST['substring'];
+
 
 $query_user = "SELECT ID FROM {$wpdb->prefix}users WHERE user_login='".$username."'";
 $result_user = $wpdb->get_results($query_user);
@@ -52,15 +36,6 @@ if(count($result_user)==0){
 //get total count of event , this is required for paging
 $countQuery = "SELECT id FROM {$wpdb->prefix}events_detail WHERE event_status != 'D'" ;
 $multi_user == true ? $countQuery .= " AND wp_user='". $userid ."' ":'';
-if($period == "today"){
-	$countQuery .= " AND start_date <= NOW() AND end_date >= NOW()";
-}
-else if($period == "upcoming"){
-	$countQuery .= " AND start_date > NOW()";
-}
-else if($period == "past"){
-	$countQuery .= " AND end_date < NOW()";
-}
 $countresult = $wpdb->get_results($countQuery);
 $count = count($countresult);
 
@@ -71,27 +46,7 @@ $query .= " FROM {$wpdb->prefix}events_detail e ";
 isset($org_options['use_venue_manager']) && $org_options['use_venue_manager'] == 'Y' ? $query .= " LEFT JOIN {$wpdb->prefix}events_venue_rel r ON r.event_id = e.id LEFT JOIN {$wpdb->prefix}events_venue v ON v.id = r.venue_id " : '';
 $query .= " WHERE e.event_status != 'D' ";
 $multi_user == true ? $query .= " AND wp_user='". $userid ."' ":'';
-if($period == "today"){
-	$query .= " AND start_date <= NOW() AND end_date >= NOW()";
-	$query .= " order by start_date asc ";
-}
-else if($period == "upcoming"){
-	$query .= " AND start_date > NOW()";
-	$query .= " order by start_date asc ";
-}
-else if($period == "past"){
-	$query .= " AND end_date < NOW()";
-	$query .= " order by start_date desc ";
-}
-
-if($use_limit == true){
-	$query .= " limit ".$perPage;
-}
-
-if($use_offset == true){
-	$query .= " offset ".$offset;
-}
-
+$query .= " AND event_name like '%".$substring."%'";
 $result = $wpdb->get_results($query);
 
 if(count($result)==0){
@@ -105,8 +60,8 @@ foreach ($result as $row) {
 		$response .= "<event_code>".$row->event_code."</event_code>";
 		$response .= "<event_name><![CDATA[".$row->event_name."]]></event_name>";
 		$response .= "<event_identifier>".$row->event_identifier."</event_identifier>";
-		$response .= "<start_date>".$row->start_date."</start_date>";
-		$response .= "<end_date>".$row->end_date."</end_date>";
+		$response .= "<start_date>".date( 'F j, Y', strtotime($row->start_date) )."</start_date>";
+		$response .= "<end_date>".date('F j, Y', strtotime($row->end_date) )."</end_date>";
 		$response .= "<venue_title><![CDATA[".$row->venue_name."]]></venue_title>";
 		$response .= "</event>";
 }
